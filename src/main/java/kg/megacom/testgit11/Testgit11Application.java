09c8d99df891ac648a05707f2632685c753e2b15
Dolon Askarbekov <dolon_aa@mail.ru>
2020-03-27 19:14:17 +0600

    initial commit

---
+++ src/main/java/kg/megacom/testgit11/Testgit11Application.java
@@ -0,0 +1,13 @@
+package kg.megacom.testgit11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testgit11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testgit11Application.class, args);
+    }
+
+}
---
+++ src/test/java/kg/megacom/testgit11/Testgit11ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.megacom.testgit11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testgit11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
